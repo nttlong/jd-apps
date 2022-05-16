5d156dae14b8e21534398adb8f99f0be00a6e6bb
nttlong <zugeliang2000@gmail.com>
2022-05-16 11:26:27 +0700

    ok

--- api_files/urls.py
+++ api_files/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from django.conf.urls import include, url
 from . import views,upload
 
-urlpatterns = [
+urlpatterns = (
     url(r'(?P<app_name>[\w\-]+)/source/(?P<upload_id>[\w\-]+).*', views.source, name='source'),
     url(r'(?P<app_name>[\w\-]+)/directory/(?P<full_path>.*)$', views.directory, name='unique_slug'),
-    url(r'registerupload$', upload.upload_register, name='unique_slug'),
-]
+    url(r'(?P<app_name>[\w\-]+)/registerupload$', upload.upload_register, name='unique_slug'),
+)
--- web/urls.py
+++ web/urls.py
@@ -19,12 +19,17 @@ from django.conf.urls import url,re_path
 from django.urls import include, path,URLPattern
 import ReEngine
 
-
+import api_files.urls
 urlpatterns = [
-    path(r'admin/', admin.site.urls)
+    path(r'admin/',admin.site.urls)
 ]
+
+#
 for app_name in ReEngine.info["APPS"].keys():
-    urlpatterns.append(
-       path(ReEngine.info["APPS"][app_name],include("{}.urls".format(app_name)))
-    )
+    try:
+        urlpatterns.append(
+           path(ReEngine.info["APPS"][app_name],include("{}.urls".format(app_name)))
+        )
+    except Exception as e:
+        print(f"load url of {app_name} is fail {e}")
 # handler404 = 'icase.views.icase_404_handler'
